6eb8a220336f9fe1ba8bc7a6a1ee73b1b5af3d99
chandan-1427 <chandandakka@gmail.com>
2026-03-16 17:00:51 +0530

    fix: use specific mypy ignore code for bindu import

--- ai_teaching_agent/main.py
+++ ai_teaching_agent/main.py
@@ -7,7 +7,7 @@ import re
 from pathlib import Path
 from typing import Any, TypedDict
 
-from bindu.penguin.bindufy import bindufy # type: ignore
+from bindu.penguin.bindufy import bindufy  # type: ignore[import-untyped]
 from dotenv import load_dotenv
 from langchain_core.messages import HumanMessage, SystemMessage
 from langchain_openai import ChatOpenAI
